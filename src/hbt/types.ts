911d2ed2e45cba753a32d7d6e82f940ed6526258
Lencse <leventeloki@gmail.com>
2021-11-10 21:12:06 +0100

    Created hiking trail types

---
+++ src/hbt/types.ts
@@ -0,0 +1,34 @@
+export interface Point {
+    lat: number
+    lon: number
+    elevation: number
+}
+
+export interface Path {
+    points: Point[]
+}
+
+export interface HikingTrail {
+    path: Path
+}
+
+export interface Checkpoint {
+    name: string
+    sectionEndpoints: SectionEndpoint[]
+}
+
+// export interface Section {
+//     path: Path
+//     from: Checkpoint
+//     to: Checkpoint
+// }
+
+export interface StampingLocation {
+    description: string
+    position: Point
+}
+
+export interface SectionEndpoint {
+    name: string
+    stampingLocations: StampingLocation[]
+}
