380b2b75220b581ffaadbb66552f49b7421ebdac
Lencse <leventeloki@gmail.com>
2021-12-15 21:34:40 +0100

    Refactor http test

--- test/server/http/http.test.ts
+++ test/server/http/http.test.ts
@@ -1,8 +1,15 @@
 import { httpGet } from '../../../src/server/http/http'
+import { createServer } from 'http'
 
 describe('HTTP Get', () => {
     it('returns response body', async () => {
-        const html = await httpGet('https://github.com/Lencse')
-        expect(html).toMatch(/Lencse/)
+        const server = await createServer((req, res) => {
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/plain')
+            res.end('Test')
+        }).listen(3000)
+        const response = await httpGet('http://localhost:3000')
+        expect(response).toBe('Test')
+        await server.close()
     })
 })
