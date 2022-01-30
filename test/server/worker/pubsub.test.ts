dd0a98b986a7088b040e3d702e33312221aafbd0
Lencse <leventeloki@gmail.com>
2022-01-30 17:42:41 +0100

    Test pubsub sending

--- test/server/worker/pubsub.test.ts
+++ test/server/worker/pubsub.test.ts
@@ -1,8 +1,18 @@
-import { createTopic } from '../../../src/server/worker/pubsub/pubsub'
+import { createTopic, sendPubSubMessage } from '../../../src/server/worker/pubsub/pubsub'
+import { v1 as uuid } from 'uuid'
 
 describe('createTopic', () => {
     it('creates valid Pub/Sub topic object', () => {
         const topic = createTopic('testTopic')
         expect(topic.name).toMatch(/testTopic$/)
+    })
+})
+
+describe('sendPubSubMessage', () => {
+    it('sends the message to a topic', async () => {
+        const topicName = `topic-${uuid()}`
+        await expect(async () => {
+            await sendPubSubMessage(topicName)({ type: 'test', data: {} })
+        }).rejects.toThrow(/NOT_FOUND/)
     })
 })
