9098409876f207bf92c8bd59ec14c04dd51085c1
Lencse <leventeloki@gmail.com>
2022-04-29 17:38:19 +0200

    Revert "Fixed e2e test"
    This reverts commit b2c8d22c183bcacb329c1592d5beeb8e439e7af2.

--- dev/worker/purge-queue.ts
+++ dev/worker/purge-queue.ts
@@ -1,7 +1,6 @@
 import { createTopic } from '../../src/server/worker/pubsub/pubsub'
 import { config } from '../../src/server/config/config'
 import { Message } from '@google-cloud/pubsub'
-import { setTimeout } from 'timers/promises'
 
 const main = async () => {
     const topic = createTopic(config.pubsubConfig().topicName)
@@ -13,8 +12,6 @@ const main = async () => {
             console.info(`1 message purged: ${data}`)
         })()
     })
-    await setTimeout(1000)
-    process.exit()
 }
 
 void main()
