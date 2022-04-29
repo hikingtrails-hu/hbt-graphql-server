1d9061fa6f328d82e760ddc93f7158358bb3c4e2
Lencse <leventeloki@gmail.com>
2022-04-29 18:24:10 +0200

    Fixed typedefs

--- src/server/graphql/typedefs.ts
+++ src/server/graphql/typedefs.ts
@@ -2,30 +2,32 @@ import { gql } from 'apollo-server-cloud-functions'
 
 export const typeDefs = gql`
     type Point {
-        lat: Float
-        lon: Float
-        elevation: Float
+        lat: Float!
+        lon: Float!
+        elevation: Float!
     }
 
     type Path {
-        points: [Point]
+        points: [Point!]!
     }
 
     type StampingLocation {
-        name: String
-        description: String
-        position: Point
+        name: String!
+        description: String!
+        position: Point!
+        pointIdx: Int!
+        distanceInMetersFromNextStampingLocation: Float
     }
 
     type Hike {
-        name: String
-        key: String
-        path: Path
-        stampingLocations: [StampingLocation]
+        name: String!
+        key: String!
+        path: Path!
+        stampingLocations: [StampingLocation!]!
     }
 
     type Query {
-        hikes: [Hike]
-        hike(key: String): Hike
+        hikes: [Hike!]!
+        hike(key: String!): Hike!
     }
 `
--- test/unit/server/http/http.test.ts
+++ test/unit/server/http/http.test.ts
@@ -1,16 +1,18 @@
 import { httpGet } from '../../../../src/server/http/http'
 import { createServer } from 'http'
 import { replaceLoggerWithSpies } from '../test-helpers'
+import { findFreePorts } from 'find-free-ports'
 
 describe('HTTP Get', () => {
     replaceLoggerWithSpies()
     it('returns response body', async () => {
+        const port = (await findFreePorts(1))[0] as number
         const server = createServer((req, res) => {
             res.statusCode = 200
             res.setHeader('Content-Type', 'text/plain')
             res.end('Test')
-        }).listen(3000)
-        const response = await httpGet('http://localhost:3000')
+        }).listen(port)
+        const response = await httpGet(`http://localhost:${port}`)
         expect(response).toBe('Test')
         await server.close()
     })
